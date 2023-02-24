3af356623ec93e5c747727b1e32bfb256fe79841
DK2698 <dileepkumar1823@gmail.com>
2023-02-24 09:51:37 +0530

    remove from repo secret key

--- src/main/java/com/kumar/springbootaws/utilty/Constant.java
+++ src/main/java/com/kumar/springbootaws/utilty/Constant.java
@@ -4,7 +4,7 @@ public class Constant {
     public static final String USER_MODEL = "USER";
     static final String GOAL_MODEL = "GOAL";
     static final String EVENTS_MODEL = "EVENT";
-    static final String YOUR_API_KEY = "AIzaSyA_uakeV7M-cG5_2k5D93NjLb-UjRn9GlY";
+    static final String YOUR_API_KEY = "GCP_API_KEY";
     static final String REGISTER_EVENT_TEMPLATE_NAME="MyTemplate";
     static final String INVITE_EVENT_TEMPLATE_NAME="InviteTemplate";
 }
